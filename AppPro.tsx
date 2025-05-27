e5226a157803ce9482ce7581a14159ee0c4cef47
imad00001 <imadkhn230@gmail.com>
2025-05-27 23:36:21 +0530

    Your commit message

---
+++ AppPro.tsx
@@ -0,0 +1,37 @@
+import React, { JSX } from 'react';
+
+import {
+    View,
+    Text,
+    StyleSheet,
+    useColorScheme,
+} from 'react-native';
+
+function AppPro(): JSX.Element{
+    const isDarkMode = useColorScheme() === 'dark';
+
+    return(
+        <View style={styles.container}>
+            <Text style={isDarkMode ? styles.whiteText : styles.whiteText}>
+                Hello World !
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    whiteText: {
+        color: '#FFFFFF',
+        fontSize: 32,
+    },
+    darkText: {
+        color: '#000000',
+    },
+});
+
+export default AppPro;
